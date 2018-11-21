b511f38d053bc58c95a53932285d12b86bde4893
Aluno <Aluno@Digital.net>
2018-11-21 14:11:15 -0200

    novos

--- router/users.js
+++ router/users.js
@@ -1,4 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const Cryptr = require('cryptr');
-var Usuario = require('')
+const cryptr = new Cryptr('2012');
+
+router.get('/', function (req, res){
+    global.conn.request().query`select nome, email from usuario`
+})
